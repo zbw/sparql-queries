cec05fce1b6e3e841f46ad1cbb1a418b67756e7a
Joachim Neubert <j.neubert@zbw.eu>
2018-11-19 08:35:07 +0100

    Output to file; add adjust_label()

--- bin/create_missing_wikidata.pl
+++ bin/create_missing_wikidata.pl
@@ -15,6 +15,9 @@ use Path::Tiny;
 use Readonly;
 use REST::Client;
 
+# limit setting just stops output (on next run, after import, created items are
+# excluded by query)
+Readonly my $LIMIT    => 2000;
 Readonly my $ENDPOINT => 'http://zbw.eu/beta/sparql/pm20/query';
 Readonly my $QUERY =>
   path('/opt/sparql-queries/pm20/companies_missing_in_wikidata.rq');
@@ -23,10 +26,9 @@ Readonly my $SOURCE_QID => '|S248|Q36948990';      # 20th century press archives
 Readonly my $SOURCE_TITLE => '|S1476|';
 Readonly my $SOURCE_ID    => '|S4293|';
 Readonly my $RETRIEVED    => "|S813|+${TODAY}T00:00:00Z/11";
-Readonly my $LIMIT        => 2000;
-Readonly my @LANGUAGES    => qw/ de en /;
 
-Readonly my %CONFIG => (
+Readonly my @LANGUAGES => qw/ de en /;
+Readonly my %CONFIG    => (
   source_title => 'pm20Label',
   source_id    => 'pm20Id',
   label        => {
@@ -56,12 +58,27 @@ Readonly my %CONFIG => (
       precision  => '',
     },
     {
+      pid        => 'P1448',
+      var_name   => 'wdLabel',
+      value_type => 'monolingual-text',
+      lang       => 'und',
+    },
+    {
       pid        => 'P227',
       var_name   => 'gndId',
       value_type => 'literal',
     },
   ],
 );
+
+# output file derived from query file, plus date for repeated runs
+my $date    = `date +%F | tr -d "\n"`;
+my $outfile = $QUERY->parent->child('results')
+  ->child( $QUERY->basename('.rq') . ".${date}.qs.txt" );
+
+if ( -f $outfile ) {
+  die "Error: $outfile exists\n";
+}
 
 # initialize rest client
 my $client = REST::Client->new( timeout => 600 );
@@ -84,7 +101,8 @@ if ($@) {
   die "Error parsing response: ", $client->responseContent(), "\n";
 }
 
-my $count;
+my $count = 0;
+my $fh    = $outfile->openw_utf8;
 foreach my $entry ( @{ $result_data->{results}->{bindings} } ) {
 
   # Limit the numer of results
@@ -102,37 +120,50 @@ foreach my $entry ( @{ $result_data->{results}->{bindings} } ) {
     . $RETRIEVED;
 
   # new item
-  print "\nCREATE\n";
+  print $fh "\nCREATE\n";
 
   # output fields
   foreach my $field (qw/ label descr alias /) {
     next unless $CONFIG{$field};
     my $abbrev = uc( substr( $field, 0, 1 ) );
     foreach my $lang (@LANGUAGES) {
-      print "LAST|$abbrev$lang|"
-        . quote( $entry->{ $CONFIG{$field}{$lang} }{value} ) . "\n";
+      my $value = $entry->{ $CONFIG{$field}{$lang} }{value};
+
+      # transform label according to Wikidata rules
+      if ( $field eq 'label' ) {
+        $value = adjust_label( $value, 'co' );
+      }
+      print $fh "LAST|$abbrev$lang|" . quote($value) . "\n";
     }
   }
 
   # output PM20 property (without reference)
-  print 'LAST|P4293|' . quote( $entry->{ $CONFIG{source_id} }{value} ) . "\n";
+  print $fh 'LAST|P4293|'
+    . quote( $entry->{ $CONFIG{source_id} }{value} ) . "\n";
 
   # output properties
   foreach my $property ( @{ $CONFIG{properties} } ) {
     my $value = $entry->{ $property->{var_name} }{value};
     next unless $value;
 
-    if ( $property->{value_type} eq 'literal' ) {
+    if ( $property->{value_type} eq 'item' ) {
+      ## use unquoted value
+    } elsif ( $property->{value_type} eq 'literal' ) {
       $value = quote($value);
     } elsif ( $property->{value_type} eq 'date' ) {
       $value = format_date($value);
+    } elsif ( $property->{value_type} eq 'monolingual-text' ) {
+      $value = quote( $value, $property->{lang} );
+    } else {
+      die 'Error: Unknown value_type [' . $property->{value_type} . "]\n";
     }
-    print 'LAST|'
+    print $fh 'LAST|'
       . $property->{pid} . '|'
       . $value
       . $reference_statement . "\n";
   }
 }
+print "$count statements written to $outfile\n";
 
 #####################
 
@@ -157,6 +188,41 @@ sub format_date {
   # TODO interpret different data formats
   if ( $date =~ m/^\d\d\d\d$/ ) {
     $date = "+$date-01-01T00:00:00Z/9";
+  } else {
+    warn "Unknown date format in [$date] - returning string\n";
+    $date = "\"$date\"";
   }
+
   return $date;
+}
+
+sub adjust_label {
+  my $label = shift or die "param missing\n";
+  my $ltype = shift or die "param missing\n";
+
+  # for companies
+  if ( $ltype eq 'co' ) {
+    $label =~ s/(.*) Ltd\.$/$1/g;
+    $label =~ s/(.*) Ltd$/$1/g;
+    $label =~ s/(.*) Limited$/$1/g;
+    $label =~ s/(.*) A\.G\.$/$1/g;
+    $label =~ s/(.*) AG$/$1/g;
+    $label =~ s/(.*) Aktiengesellschaft$/$1/g;
+    $label =~ s/(.*) \& Co\.$/$1/ig;
+    $label =~ s/(.*) \& Co$/$1/ig;
+    $label =~ s/(.*) Co\.$/$1/ig;
+    $label =~ s/(.*) Co$/$1/ig;
+    $label =~ s/(.*) Corpi\.$/$1/ig;
+    $label =~ s/(.*) mbh$/$1/ig;
+    $label =~ s/(.*) Gmbh$/$1/ig;
+    $label =~ s/(.*) e.V.$/$1/ig;
+    $label =~ s/(.*) plc\.$/$1/g;
+    $label =~ s/(.*) plc$/$1/g;
+    $label =~ s/(.*) SA$/$1/g;
+    $label =~ s/(.*) S\.A\.$/$1/g;
+    $label =~ s/(.*) NV$/$1/g;
+    $label =~ s/(.*) N\.V\.$/$1/g;
+    $label =~ s/(.*) Co\.,$/$1/g;
+  }
+  return $label;
 }
