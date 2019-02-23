355e69ab26efa9de0cdba5b03bc8232ecf5c75e8
Joachim Neubert <j.neubert@zbw.eu>
2019-02-23 07:09:18 +0100

    Extend pm20 type count statistics

--- bin/pm20_stats.sh
+++ bin/pm20_stats.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
+ENDPOINT=http://zbw.eu/beta/sparql/pm20/query
+QUERY=count_folder_type.rq
+
 cd /opt/sparql-queries/pm20
-curl --silent -X POST -H "Accept: application/sparql-results+json" --data "query=`cat count_folder_type.rq`"  https://query.wikidata.org/sparql > results/count_folder_type.json
-curl --silent -X POST -H "Accept: text/tab-separated-values" --data "query=`cat count_folder_type.rq`"  https://query.wikidata.org/sparql > results/count_folder_type.tsv
+curl --silent -X POST -H "Content-type: application/sparql-query" -H "Accept: application/sparql-results+json" --data-binary @$QUERY $ENDPOINT > results/count_folder_type.json
+curl --silent -X POST -H "Content-type: application/sparql-query" -H "Accept: text/tab-separated-values" --data-binary @$QUERY $ENDPOINT > results/count_folder_type.tsv
 
