a12fcf73c65aa7cbed736344f87022781d2bfa9c
Joachim Neubert <j.neubert@zbw.eu>
2019-02-26 07:05:01 +0100

    Prepare for regularly created stats

--- bin/pm20_stats.sh
+++ bin/pm20_stats.sh
@@ -4,6 +4,10 @@ ENDPOINT=http://zbw.eu/beta/sparql/pm20/query
 QUERY=count_folder_type.rq
 
 cd /opt/sparql-queries/pm20
+
+# 
 curl --silent -X POST -H "Content-type: application/sparql-query" -H "Accept: application/sparql-results+json" --data-binary @$QUERY $ENDPOINT > results/count_folder_type.json
 curl --silent -X POST -H "Content-type: application/sparql-query" -H "Accept: text/tab-separated-values" --data-binary @$QUERY $ENDPOINT > results/count_folder_type.tsv
+
+git commit -m "Update stats" results/count_folder_type.*
 
