26e55fa7057c2d98003c8e19ae51b5309252025e
Joachim Neubert <j.neubert@zbw.eu>
2020-06-26 21:51:39 +0200

    URL changed to toolforge

--- bin/mnm2graph.pl
+++ bin/mnm2graph.pl
@@ -14,10 +14,8 @@ use Data::Dumper;
 use Readonly;
 use REST::Client;
 
-##Readonly my $MNM_URL_TEMPLATE =>
-##'http://tools.wmflabs.org/mix-n-match/api.php?query=download2&catalogs=CATALOG_ID&columns=%7B%22exturl%22%3A1%2C%22username%22%3A1%2C%22aux%22%3A0%2C%22dates%22%3A0%2C%22location%22%3A0%2C%22multimatch%22%3A1%7D&hidden=%7B%22any_matched%22%3A0%2C%22firmly_matched%22%3A0%2C%22user_matched%22%3A0%2C%22unmatched%22%3A0%2C%22automatched%22%3A0%2C%22name_date_matched%22%3A0%2C%22aux_matched%22%3A0%2C%22no_multiple%22%3A0%7D&format=tab&as_file=1';
 Readonly my $MNM_URL_TEMPLATE =>
-'https://tools.wmflabs.org/mix-n-match/api.php?query=download2&catalogs=CATALOG_ID&as_file=1';
+  'https://mix-n-match.toolforge.org/api.php?query=download2&catalogs=CATALOG_ID&as_file=1';
 Readonly my %URI => (
   gnd  => 'https://d-nb.info/gnd/',
   pm20 => 'http://purl.org/pressemappe20/folder/',
