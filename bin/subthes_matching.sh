c551a8754d961f8a8146e857ffd2332388ae4846
Joachim Neubert <j.neubert@zbw.eu>
2021-12-23 08:23:22 +0100

    Deactivate creation of OR input

--- bin/subthes_matching.sh
+++ bin/subthes_matching.sh
@@ -5,6 +5,7 @@
 
 SEARCH=B.01.01.01
 for replace in B.01 B.02 B.03 B.04 B.05 B.06 B.07 B.08 B.09 B.10 ; do 
-  ./make_openrefine_input.sh stw stw_openrefine $SEARCH $replace
+  ## activate only for initial production
+  ##./make_openrefine_input.sh stw stw_openrefine $SEARCH $replace
   perl missing_wd_item_from_stw.pl $SEARCH $replace
 done
